2eda3bf4581a57967255e81c2f2b0b7eea6f2b91
Sahand Mosharafian <82970651+sahandmsh@users.noreply.github.com>
2023-09-12 17:39:27 -0700

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Sahand Mosharafian on 9/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
